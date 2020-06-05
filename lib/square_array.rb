9ea964d55f82805f9c9e82c161e264314f7f942f
Donat Pllana <donat.pllana@outlook.com>
2020-06-05 19:26:06 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,8 @@
 def square_array(array)
-  # your code here
   new_array=[]
   counter=0
   while counter<array.length do
-    new_array.push(Math.sqrt(array(counter)))
+    new_array.push(Math.sqrt(array[counter]))
   end
   new_array
 end
