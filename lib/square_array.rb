a21b9fc4ed3bee429f17620759d9304b57ab2554
Donat Pllana <donat.pllana@outlook.com>
2020-06-05 19:29:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_array=[]
   counter=0
   while counter<array.length do
-    new_array.push(Math.pow(array[counter]))
+    new_array.push(array[counter]**2)
     counter+=1
   end
   new_array
